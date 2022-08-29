8c02daf40bb8cc5e2ea41326d0a9847c0de289ca
Abhinav Singh <abhinav12.singh@paytm.com>
2022-08-29 16:05:25 +0530

    added expense model to store users expense data

--- api/models.py
+++ api/models.py
@@ -1,5 +1,22 @@
-from __future__ import unicode_literals
-
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(default=datetime.utcnow)
+    updated_at = models.DateTimeField()
+
+class ExpenseUser(models.Model):
+    contact_number = models.IntegerField(null=False, primary_key=True)
+
+class Expense(models.Model):
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    description = models.CharField(max_length=512, null=True, blank=True)
+    expense_date = models.DateTimeField(null=False, default=datetime.utcnow)
+    category = models.CharField(max_length=100, null=False)
+    user = models.ForeignKey(to=ExpenseUser, on_delete=models.CASCADE)
+
